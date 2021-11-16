09d596c96abe65b34a0645a354ca2e57c46aebd4
Aswin nk <nkaswin10@gmail.com>
2021-11-16 13:29:55 +0530

    initialised flask name and first methode

--- app.py
+++ app.py
@@ -3,3 +3,9 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import train_test_split
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
