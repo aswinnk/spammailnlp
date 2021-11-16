8552c80e5ce6de1e657e88f717fc02e7aef9fc8a
Aswin nk <nkaswin10@gmail.com>
2021-11-16 13:52:43 +0530

    add run for flask

--- app.py
+++ app.py
@@ -36,3 +36,6 @@ def sampredict():
         vect = cv.transform(data).toarray()
         my_prediction = clf.predict(vect)
     return render_template('index.html', prediction=my_prediction)
+
+if __name__ == '__main__':
+    app.run()    
