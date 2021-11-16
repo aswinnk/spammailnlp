55c2efdd8b0194fd82ec400768f3becc8adefa0a
Aswin nk <nkaswin10@gmail.com>
2021-11-16 13:44:31 +0530

    extract the feature with countvectoriser and transform

--- app.py
+++ app.py
@@ -6,7 +6,20 @@ from sklearn.model_selection import train_test_split
 
 app = Flask(__name__)
 
+
+#first methode
 @app.route('/')
 def home():
     return render_template('index.html')
+
+#second methode
+
+@app.route('/spampredict', methods=['POST'])
+def sampredict():
+    df = pd.read_csv("spam.csv", encoding="latin-1")
+    df.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], axis=1, inplace=True)
+
+    df['label'] = df['class'].map({'ham': 0, 'spam': 1})
+    X = df['message']
+    y = df['label']
     
