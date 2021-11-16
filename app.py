a951f826ad38b99cbf7a7f0bb2e0c738b96bdef7
Aswin nk <nkaswin10@gmail.com>
2021-11-16 13:51:33 +0530

    index page edited and add naive bayes for prediction algorithm

--- app.py
+++ app.py
@@ -22,4 +22,17 @@ def sampredict():
     df['label'] = df['class'].map({'ham': 0, 'spam': 1})
     X = df['message']
     y = df['label']
+
+    cv = CountVectorizer()
+    X = cv.fit_transform(X) 
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
     
+    clf = MultinomialNB()
+    clf.fit(X_train, y_train)
+    clf.score(X_test, y_test)
+    if request.method == 'POST':
+        message = request.form['message']
+        data = [message]
+        vect = cv.transform(data).toarray()
+        my_prediction = clf.predict(vect)
+    return render_template('index.html', prediction=my_prediction)
