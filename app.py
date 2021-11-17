305a024b8945e1c543a1bd0c533c49601660df2c
Aswin nk <nkaswin10@gmail.com>
2021-11-17 22:15:01 +0530

    all set

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def sampredict():
 
     cv = CountVectorizer()
     X = cv.fit_transform(X) 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=5500)
     
     clf = MultinomialNB()
     clf.fit(X_train, y_train)
